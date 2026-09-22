99051d56e785d2b53d218b993c5859bbc61f1c0c
ikrti <minipro787@gmail.com>
2026-09-22 15:45:09 +0700

    fix: proxy api v1 directly

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
   reactStrictMode: true,
+  async rewrites() {
+    const backendUrl = process.env.BACKEND_URL ?? "http://localhost:3000/api/v1";
+
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${backendUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
